9bf9ad2a74e39cf34107ac6a7f74c0066bb7926c
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2021-07-12 12:33:08 +0100

    UI: Create basic layout

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
 use std::io;
 use tui::Terminal;
-use tui::backend::CrosstermBackend;
+use tui::backend::{CrosstermBackend, Backend};
 use tui::widgets::{Widget, Block, Borders};
 use tui::layout::{Layout, Constraint, Direction};
 
+
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
+    let mut backend = CrosstermBackend::new(stdout);
+    backend.clear()?;
     let mut terminal = Terminal::new(backend)?;
     terminal.draw(|f| {
         let size = f.size();
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(80),
+                ].as_ref()
+            )
+            .split(size);
+
+        let heirarchy = Block::default()
+            .title("Accounts")
+            .borders(Borders::RIGHT);
+        let render = Block::default();
+
         // Create a bordered block
-        let block = Block::default()
+        let window = Block::default()
             .title("Greymail")
             .borders(Borders::ALL);
-        f.render_widget(block, size);
-    })
 
+        f.render_widget(window, size);
+        f.render_widget(heirarchy, chunks[0]);
+        f.render_widget(render, chunks[1]);
+    })?;
+    Ok(())
 }
